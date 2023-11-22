89a2d4057616e103b1ea342d9fd3e191b6d723f3
gbakradze94 <Bakradzegeorge17@gmail.com>
2023-11-22 11:45:32 +0400

    Refactor codestyle

--- searching-challenges/src/main/java/org/doomsday/searching/BinarySearch.java
+++ searching-challenges/src/main/java/org/doomsday/searching/BinarySearch.java
@@ -4,14 +4,14 @@ public class BinarySearch {
     public int binarySearchRecursive(int[] array, int key, int low, int high) {
         int middle = low + ((high - low) / 2);
 
-        if(high < low) {
+        if (high < low) {
             return -1;
         }
-        if(key == array[middle]) {
+        if (key == array[middle]) {
             return middle;
-        }else if (key < array[middle]) {
+        } else if (key < array[middle]) {
             return binarySearchRecursive(array, key, low, middle -1);
-        }else {
+        } else {
             return binarySearchRecursive(array, key, middle + 1, high);
         }
     }
