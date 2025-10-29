cceaa3007a9db5d86c74f57c1d36c2768e5baf12
Gbakradze94 <66497177+Gbakradze94@users.noreply.github.com>
2025-10-29 21:47:15 +0400

    Sequenced collections (#15)
    * Add materials
    
    * Update materials
    
    * Add demo code for sequenced collection

---
+++ java25/src/main/java/org/gbakradze/B.java
@@ -0,0 +1,15 @@
+package org.gbakradze;
+
+public class B  extends A {
+
+    public static final String b = "class B" ;
+
+    public static  int i = 0 ;
+
+    static{
+
+        i = Integer.parseInt("23");
+
+    }
+
+}
